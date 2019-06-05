532d15a5fb2de5b02107b95b160402dfc6aae1e5
VivekV95 <vivekv210@gmail.com>
2019-06-05 16:57:12 -0500

    Added beans for message queues

--- dogs-initial/src/main/java/com/lambdaschool/dogsinitial/DogsinitialApplication.kt
+++ dogs-initial/src/main/java/com/lambdaschool/dogsinitial/DogsinitialApplication.kt
@@ -1,7 +1,13 @@
 package com.lambdaschool.dogsinitial
 
+import org.springframework.amqp.core.Binding
+import org.springframework.amqp.core.BindingBuilder
+import org.springframework.amqp.core.Queue
+import org.springframework.amqp.core.TopicExchange
+import org.springframework.amqp.support.converter.Jackson2JsonMessageConverter
 import org.springframework.boot.SpringApplication
 import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.context.annotation.Bean
 import org.springframework.web.servlet.DispatcherServlet
 import org.springframework.web.servlet.config.annotation.EnableWebMvc
 
@@ -13,6 +19,10 @@ open class DogsinitialApplication {
     companion object {
         lateinit var ourDogList: DogList
 
+        val EXCHANGE_NAME = "LambdaServer"
+        val QUEUE_NAME_LOW = "LowPriorityQueue"
+        val QUEUE_NAME_HIGH = "HighPriorityQueue"
+
         @JvmStatic
         fun main(args: Array<String>) {
             ourDogList = DogList()
@@ -22,4 +32,34 @@ open class DogsinitialApplication {
             dispatcherServlet.setThrowExceptionIfNoHandlerFound(true)
         }
     }
+
+    @Bean
+    open fun appExchange(): TopicExchange {
+        return TopicExchange(EXCHANGE_NAME)
+    }
+
+    @Bean
+    open fun appQueueLow(): Queue {
+        return Queue(QUEUE_NAME_LOW)
+    }
+
+    @Bean
+    open fun declareBindingLow(): Binding {
+        return BindingBuilder.bind(appQueueLow()).to(appExchange()).with(QUEUE_NAME_LOW)
+    }
+
+    @Bean
+    open fun appQueueHigh(): Queue {
+        return Queue(QUEUE_NAME_HIGH)
+    }
+
+    @Bean
+    open fun declareBindingHigh(): Binding {
+        return BindingBuilder.bind(appQueueHigh()).to(appExchange()).with(QUEUE_NAME_HIGH)
+    }
+
+    open fun produceJackson2MessageConverter(): Jackson2JsonMessageConverter {
+        return Jackson2JsonMessageConverter()
+    }
+
 }
