19073aca58f1e4c5275cc59f1fd4443376dd0d0f
VivekV95 <vivekv210@gmail.com>
2019-06-05 17:15:25 -0500

    Added message queues for /dogs and /dogs/$id endpoints

--- dogs-initial/src/main/java/com/lambdaschool/dogsinitial/controller/DogController.kt
+++ dogs-initial/src/main/java/com/lambdaschool/dogsinitial/controller/DogController.kt
@@ -5,9 +5,12 @@ import com.lambdaschool.dogsinitial.CheckDog
 import com.lambdaschool.dogsinitial.model.Dog
 import com.lambdaschool.dogsinitial.DogsinitialApplication
 import com.lambdaschool.dogsinitial.exception.ResourceNotFoundException
+import com.lambdaschool.dogsinitial.model.MessageDetail
 import mu.KotlinLogging
 import org.slf4j.Logger
 import org.slf4j.LoggerFactory
+import org.springframework.amqp.rabbit.core.RabbitTemplate
+import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.http.HttpStatus
 import org.springframework.http.ResponseEntity
 import org.springframework.ui.Model
@@ -24,14 +27,23 @@ import java.util.*
 @RestController
 @RequestMapping("/dogs")
 class DogController {
+
     companion object{
         private val logger = KotlinLogging.logger{}
     }
+
+    @Autowired
+    lateinit var rt: RabbitTemplate
+
     // localhost:8080/dogs/dogs
     val allDogs: ResponseEntity<*>
         @GetMapping(value = ["/dogs"], produces = ["application/json"])
         get() {
-            logger.info { "dogs/dogs accessed on ${SimpleDateFormat("dd MMM yyyy HH:mm:ss:SSS Z").format(Date())}" }
+            logger.info { "dogs/dogs accessed" +
+                    " on ${SimpleDateFormat("dd MMM yyyy HH:mm:ss:SSS Z").format(Date())}" }
+            val message = MessageDetail("/dogs/dogs accessed on" +
+                    " ${SimpleDateFormat("dd MMM yyyy HH:mm:ss:SSS Z").format(Date())}", 7, false)
+            rt.convertAndSend(DogsinitialApplication.QUEUE_NAME_HIGH, message)
             return ResponseEntity(DogsinitialApplication.ourDogList.dogList, HttpStatus.OK)
         }
 
@@ -41,6 +53,9 @@ class DogController {
     @GetMapping(value = ["/{id}"], produces = ["application/json"])
     fun getDogDetail(@PathVariable id: Long): ResponseEntity<*> {
         logger.info { "dogs/$id accessed on ${SimpleDateFormat("dd MMM yyyy HH:mm:ss:SSS Z").format(Date())}" }
+        val message = MessageDetail("/dogs/$id accessed on" +
+                " ${SimpleDateFormat("dd MMM yyyy HH:mm:ss:SSS Z").format(Date())}", 2, true)
+        rt.convertAndSend(DogsinitialApplication.QUEUE_NAME_LOW, message)
         val rtnDog = DogsinitialApplication.ourDogList.findDog(CheckDog
         { d -> d.id == id }) ?: throw ResourceNotFoundException("Dog with id $id not found")
         return ResponseEntity(rtnDog, HttpStatus.OK)
--- dogs-initial/src/main/java/com/lambdaschool/dogsinitial/model/MessageDetail.kt
+++ dogs-initial/src/main/java/com/lambdaschool/dogsinitial/model/MessageDetail.kt
@@ -1,8 +1,10 @@
 package com.lambdaschool.dogsinitial.model
 
+import java.io.Serializable
+
 data class MessageDetail(var text: String? =null ,
                          var priority: Int? = null,
-                         var secret: Boolean? = null) {
+                         var secret: Boolean? = null): Serializable {
     override fun toString(): String {
         return "MessageDetail(text=$text, priority=$priority, secret=$secret)"
     }
